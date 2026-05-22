71d6f03048acb2643d5083279660e0c684c3fdf0
Muhammad Bilal Nasir <bilalnasireducation@gmail.com>
2026-05-22 22:10:56 +0500

    Add video routes

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { getAllVideos } from "../controllers/video.controller.js";
+import { deleteVideo, getAllVideos, getVideoById, publishVideo, togglePublishStatus, updateVideo } from "../controllers/video.controller.js";
 
 const router = Router();
 router.use(verifyJWT);
@@ -8,5 +8,15 @@ router.use(verifyJWT);
 router
     .route("/")
     .get(getAllVideos);
+
+router.route('/publish-video').post(publishVideo);
+
+router.route('/get-video').get(getVideoById);
+
+router.route('/update-video/:videoId').patch(updateVideo);
+
+router.route('/delete-video/:videoId').delete(deleteVideo);
+
+router.route('/toggle-publish-status/:videoId').patch(togglePublishStatus);
 
 export default router;
