d950e7585e3c611a0d74cc2f46f3a6f37baeb31a
Muhammad Bilal Nasir <bilalnasireducation@gmail.com>
2026-05-27 20:42:10 +0500

    feat: add getUserPlaylists and getPlaylistById controllers with validation

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -2,6 +2,7 @@ import {asyncHandler} from "../utils/asyncHandler.js";
 import {ApiResponse} from "../utils/ApiResponse.js";
 import {ApiError} from "../utils/ApiError.js";
 import { Playlist } from "../models/playlist.model.js";
+import mongoose from "mongoose";
 
 const createPlaylist = asyncHandler(async (req, res) => {
     const { name, description } = req.body;
@@ -34,6 +35,53 @@ const createPlaylist = asyncHandler(async (req, res) => {
     );
 })
 
+const getUserPlaylists = asyncHandler(async (req, res) => {
+    const {userId} = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(userId)) {
+        throw new ApiError(400, 'Invalid userId');
+    };
+
+    const playlists = await Playlist.find({
+        owner: userId
+    });
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            playlists,
+            'Playlists fetched successfully'
+        )
+    );
+})
+
+const getPlaylistById = asyncHandler(async (req, res) => {
+    const {playlistId} = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(playlistId)) {
+        throw new ApiError(400, 'Invalid playlistId');
+    };
+
+    const playlist = await Playlist.findById(playlistId);
+    if(!playlist) {
+        throw new ApiError(500, 'Error fetching playlist')
+    };
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            playlist,
+            'Playlist fetched successfully'
+        )
+    );
+
+    
+
+})
 export {
     createPlaylist
 }
