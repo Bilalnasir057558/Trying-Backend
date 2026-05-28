d41725899cee597aec16ca5bb2964033322a1e84
Muhammad Bilal Nasir <bilalnasireducation@gmail.com>
2026-05-28 17:37:18 +0500

    feat: add toggle like functionality for comments

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -3,6 +3,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { Video } from "../models/video.model.js";
 import { Like } from "../models/like.model.js";
+import { Comment } from "../models/comment.model.js";
 
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
@@ -44,8 +45,46 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
     );
 })
 
-const 
+const toggleCommentLike = asyncHandler(async (req, res) => {
+    const {commentId} = req.params;
+
+    const comment = await Comment.findById(commentId);
+    if(!comment) {
+        throw new ApiError(404, "Comment not found")
+    };
+
+    // check if the comment is already liked or not
+    const isLiked = await Like.findOne({
+        comment: commentId,
+        likedBy: req.user._id
+    });
+
+    let likedComment;
+    if(!isLiked) {
+        likedComment = await Like.create({
+            comment: commentId,
+            likedBy: req.user._id
+        })
+    } else {
+        await Like.deleteOne({
+            comment: commentId,
+            likedBy: req.user._id
+        })
+    };
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            isLiked ? likedComment : {},
+            isLiked ? 'Like added to comment successfully' : 'Like removed from the comment successfully'
+        )
+    );
+
+})
 
 export {
-    toggleVideoLike
+    toggleVideoLike,
+    toggleCommentLike
 }
--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import {verifyJWT} from "../middlewares/auth.middleware.js"
-import { toggleVideoLike } from "../controllers/like.controller";
+import { toggleCommentLike, toggleVideoLike } from "../controllers/like.controller.js";
 
 const router = Router();
 router.use(verifyJWT);
 
 router.route('/like-video/:videoId').post(toggleVideoLike);
+router.route('/like-comment/:commentId').post(toggleCommentLike);
 
 export default router
